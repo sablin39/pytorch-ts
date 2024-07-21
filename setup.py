b3273086eca6edceb1dcd9983092bd99d08b094e
sablin39 <1020030829@qq.com>
2024-07-21 23:21:17 +0800

    Fix some bugs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pytorchts",
-    version="0.6.0-gluonts0.12.8",
+    version="0.6.0",
     description="PyTorch Probabilistic Time Series Modeling framework",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
